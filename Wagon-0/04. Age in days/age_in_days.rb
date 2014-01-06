083f0fc88bdf215a16c3229550060db2ebb4a383
Laurent Cuzin <laurentcuzin@gmail.com>
2014-01-06 17:54:15 +0100

    solution to ruby-0 challenge 4

--- Wagon-0/04. Age in days/age_in_days.rb
+++ Wagon-0/04. Age in days/age_in_days.rb
@@ -1,7 +1,11 @@
 def age_in_days(day, month, year)
-  # your code goes here !
+	today_in_sec = Time.now.to_i
+	birth_in_sec = Time.new(year, month, day).to_i
+	difference = today_in_sec-birth_in_sec
+	days = difference/86400
+	"you have #{days} days buddy"
 end
-
 
 # Testing your code
 puts age_in_days(6, 11, 1985)
+
