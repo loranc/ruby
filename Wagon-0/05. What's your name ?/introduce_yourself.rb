0aea01d8408838d7b71a8824f14315a474ebc61c
Laurent Cuzin <laurentcuzin@gmail.com>
2014-01-06 19:02:11 +0100

    soluce ex. 05

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,24 @@
 # your code goes here, introduce yourself !
+puts "first_name?"
+first_name = gets.chomp
+puts "middle_name?"
+middle_name = gets.chomp
+puts "last name?"
+last_name = gets.chomp
+
+fullname = "#{first_name} #{middle_name} #{last_name}"
+fullname_character = fullname.length
+
+puts "Hello," + fullname
+puts "Hello," + " " + first_name + " " + middle_name + " " + last_name
+
+
+puts "fullname has got #{fullname_character} characters, including spaces"
+
+if fullname == "laurent jean cuzin"
+	puts "You're a good person"
+	elsif fullname == "Boris Alexandre Papillard"
+	puts "You're a good person too"
+	else
+end
+
