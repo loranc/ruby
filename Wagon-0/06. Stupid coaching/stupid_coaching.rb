65a0082f0d3ecfdb4f5733f7c9ed92e76b02fff4
Laurent Cuzin <laurentcuzin@gmail.com>
2014-01-07 15:29:55 +0100

    soluce exercice 6 et 7

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,6 +1,54 @@
-def stupid_coaching 
-  # your code goes here
+def first_answer(answer)
+	if answer.end_with?("?") 
+		puts "Silly question ! Get dressed and go to work !"
+	else 
+		puts "I don't care son"
+	end
 end
 
-# Calling the method
+def second_answer(answer)
+		
+		if answer.end_with?("?") 
+		puts "Silly question, take the subway and go to work !"
+		else 
+		puts "I don't care son, take the subway and go to work !"
+		end
+		
+end
+
+def shout(answer)
+		if answer == answer.upcase
+		puts "I can feel your motivation son !"
+		else
+		end
+end
+
+def stupid_coaching 
+	
+	puts "Hey son"
+	has_worked = false
+
+	while true
+		answer = gets.chomp
+		shout(answer)
+		break if answer == "I'm bored !"
+			
+		if answer == "I'm going to work"
+			puts "Good boy"
+			has_worked = true
+			next   		
+		end
+		
+		if has_worked 
+		second_answer(answer)
+		else 
+		first_answer(answer)
+		end
+
+	end
+
+end
+
+
 stupid_coaching
+	
